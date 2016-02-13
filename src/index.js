c26234bb727329d64052cc4dd8e7c8b4af2e9bc7
Paul Stahmer <pstahmer25@gmail.com>
2016-02-13 00:23:54 -0600

    extracted slack to an importable file, moved trigger registration to that file as well

--- src/index.js
+++ src/index.js
@@ -1,29 +1,16 @@
-import Slack from "slack-client";
+import slack from "./slack";
 import mongoose from "mongoose";
 import _ from "lodash";
 
 import * as plugins from "./plugins";
+import { checkTriggers } from "./utils/triggers";
 
-const slackToken = "xoxb-21249912663-suwrlsa6J7i5llfbhw7aHert";
-const autoReconnect = true; // Automatically reconnect after an error response from Slack.
-const autoMark = true; // Automatically mark each message as read after it is processed.
-
-const slack = new Slack(slackToken, autoReconnect, autoMark);
-
-slack.on("open", () => {
-  console.log(`Connected to ${slack.team.name} as ${slack.self.name}`);
-});
-
-slack.on("message", (message) => {
-});
-
-slack.on("error", (err) => {
-});
-
-slack.login();
 mongoose.connect('mongodb://localhost/my_database');
 
 // now that we have initialized our slack object, load all our plugins into the bot
-_.each(plugins, (p) => {
-  p(slack);
+_.each(plugins, (triggers) => {
+  slack.on("message", (message) => {
+    //console.log(message);
+    checkTriggers(slack, triggers, message)
+  });
 });
