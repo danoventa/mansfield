69a4d3243fd404eeed75c990a7be55ee33ea9ec8
Paul Stahmer <pstahmer25@gmail.com>
2016-02-13 15:24:25 -0600

    fixed issue with deleted messages crashing bot

--- src/utils/triggers.js
+++ src/utils/triggers.js
@@ -4,7 +4,7 @@ export const checkTriggers = (slack, triggers, message) => {
   // assume any caller is smart enough to only have one valid trigger per statement
   _.each(triggers, (t) => {
     const trigger = `${slack.self.name} ${t.trigger}`;
-    if (message.text.indexOf(trigger) === 0) {
+    if (message.text && message.text.indexOf(trigger) === 0) {
       // trim out the trigger for clarity in the action
       message.text = message.text.substr(trigger.length + 1);
       return t.action(message, slack);
